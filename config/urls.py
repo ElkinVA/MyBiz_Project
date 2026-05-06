573d3439bcd7508d350487a0a4d47b4749a83576
ElkinVA <v.a.yolkin@gmail.com>
2026-05-06 13:29:01 +0300

    060520261328

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from config.admin import admin  # Импортируем кастомный админ-сайт
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
